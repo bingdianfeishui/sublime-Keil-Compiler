721391d4f0dc3b46e2dc6c75782eb3562ddd6181
bingdianfeishui <bingdianfeishui@hotmail.com>
2016-06-09 09:40:20 +0800

    增加修改.uvproj文件的功能

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # Sublime Keil Compiler
-This is Sublime Text Plugin for embedded developer with Keil C, Sublime project must upon or beside the keil project path, Compiling log file will jump up when the Compile is done, click Tools->keil Compiler or alt+C compile
 
-這是我自己做的一套sublime Text plugin, 因為自己平常不會使用keil來編輯, 所以寫了一套簡單的編譯器方便在sublime Text上直接編譯
+**在Sublime Text 3 及 Keil uVision 4下测试通过，其余版本未测试。**
 
-只要sublime跟keil在同一個文件底下就可以直接編譯, 編譯完之後就會跳出keil的執行結果, 如果有任何意見或是建議歡迎提出
+Compile the Keil project in Sublime Text. Let the ugly keil editor go away.
 
-#Install
-1.add keil UV4 to your Environment Variables PATH (ex:C:\Keil\UV4)
+Keil的自带编辑器很难用，所以我利用Sublime Text作为Keil uVision的外部编辑器来学习MCS51单片机，但每次编辑完成后都需要关闭sublime回到Keil中进行编译，不是很方便，要是sublime能直接编译C51文件就好了。网上有一些用sublime调用C51的编译器、连接器等进行编译的方法，但设置麻烦，而且相对的不太容易传递编译参数。幸运的是我在github上发现了[stu0219/sublime-Keil-Compiler](https://github.com/stu0219/sublime-Keil-Compiler)，直接在sublime中以命令行的方式调用Keil uVision进行编译。但由于我学习C51单片机时，经常需要在Keil的Project中切换.c文件，每次都要到Keil中设置，所以我Fork了这个项目，添加了编译前更改Keil Project文件的功能，这样就基本不用回到Keil中做任何设置，全部在Sublime中进行，方便太多了~~
 
-2.Unzip whole file to sublime text path ..\Data\Packages\(file)
-
-3.prepare keil file(build project and add file)
-
-4.build sublime text project and keil project in the same folder
+##Install
+1. 安装Keil软件，然后在系统的环境变量`Path`中添加你的Keil路径，如`C:\Keil\UV4`。
+   Install Keil uVision, then add the keil path to the environment variables `Path`, ex: `C:\Keil\UV4`.
+2. 复制所有文件到`Data\Packages\`文件夹下，当然你可以自己建一个子文件夹来保存。关于Data文件夹的位置，请参见[The *Data* Directory](http://docs.sublimetext.info/en/latest/basic_concepts.html#the-data-directory)。
+   Copy all files to the `Data\Packages\`, of course, you can create a sub-folder.
+3. 打开Keil软件新建一个项目，保存。
+   Open Keil uVision to create a project, then save it.
+4. 打开Sublime Text，将Keil项目所在文件夹添加到项目，并保存项目，以便生成一个`.sublime-project`文件。
+   Open Sublime Text, add the Keil Project folder to the sublime project, and save the project for creating a `.sublime-project` file.
+5. 新建一个`.c`文件，并保存到项目文件夹中，按`Alt+C`或菜单`Tools->Keil Compiler`编译，编译完成后自动打开`log.txt`文件。查看项目文件夹，`.hex`文件是不是已经生成了呢~
+   Create a new `.c` file, input some code, save it to the sublime project folder. Compile it for `Alt+C` or `Tools->Keil Compiler`. The `log.txt` will be open when completed. Now, see your project folder, the `.hex` file is already here. Download it to your MCU. Enjoy it~
---
+++ keil_Compiler.py
@@ -0,0 +1,74 @@
+import os
+import sublime, sublime_plugin
+import subprocess, sys
+import xml.etree.cElementTree as ET 
+
+class KeilCompileCommand(sublime_plugin.TextCommand):
+	def run(self,edit):
+
+		k=self.view.window().extract_variables()
+		os.chdir(k["project_path"])							
+		path=os.listdir(os.getcwd())
+						
+		for i in path:
+		    if(i.find(".uvproj")>0):
+        		Project=i
+
+        
+		settings = sublime.load_settings('keil_Compiler.sublime-settings')
+		value = settings.get('modify_uvproj')
+		if value:
+			cur_file = os.path.basename(self.view.file_name())
+			self.modify_uvproj(Project, cur_file)
+			# print("hahahhahah")
+
+		Command="UV4 -b {0} -o log.txt".format(Project)
+		
+		subprocess.call(Command,stdout=subprocess.PIPE,shell =False )
+
+		self.view.window().open_file(k["project_path"]+"/log.txt")
+		#filea = open("log.txt","r")
+		#print (filea.read())
+		print ("saved")
+
+	# def modify_uvproj2(self, uvproj, cur_file):
+
+	# 	tree = ET.parse(uvproj)     #打开xml文档    
+
+	# 	file_node = tree.getroot().find("Targets/Target/Groups/Group/Files/File")
+	# 	# print(file_node.tag, "     ",file_node.text)
+	# 	print("修改前：",  file_node.find("FileName").text )
+	# 	file_node.find("FileName").text = cur_file
+	# 	print("修改后：",  file_node.find("FileName").text )
+
+	# 	print("修改前：",  file_node.find("FilePath").text )
+	# 	file_node.find("FilePath").text = ".\\" + cur_file
+	# 	print("修改后：",  file_node.find("FilePath").text )
+
+	# 	tree.write(uvproj)	#保存
+
+	def modify_uvproj(self, uvproj, cur_file):
+		source_str1 = "<FileName>"
+		source_str2 = "<FilePath>"
+		source_str3 = "<CreateHexFile>0</CreateHexFile>"
+		source_str4 = "<OutputName>"
+		lines=open(uvproj,'r').readlines()
+		lines_len=len(lines)-1
+		for i in range(lines_len):
+			if source_str1 in lines[i]:
+				print("old: "+lines[i])
+				lines[i]= "\t\t\t  <FileName>"+cur_file+"</FileName>\n"
+				print("new: "+lines[i])
+				continue
+			if source_str2 in lines[i]:
+				print("old: "+lines[i])
+				lines[i]= "\t\t\t  <FilePath>"+cur_file+"</FilePath>\n"
+				print("new: "+lines[i])
+				continue
+			if source_str3 in lines[i]:
+				lines[i]= lines[i].replace(source_str3, "<CreateHexFile>1</CreateHexFile>")
+				continue
+			if source_str4 in lines[i]:
+				lines[i]= "\t\t\t\t\t<OutputName>"+os.path.splitext(uvproj)[0]+"</OutputName>\n"
+				continue
+		open(uvproj,'w').writelines(lines)
