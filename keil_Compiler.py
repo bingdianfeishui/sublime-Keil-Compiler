1707c6843af3ae612e5a7746b43c899da923df87
bingdianfeishui <bingdianfeishui@hotmail.com>
2016-06-26 23:55:13 +0800

    支持一个项目多个文件
    1 支持在一个keil项目中添加多个c文件。
    	自动添加所有打开的.c或.C文件到项目。
    2 采用minidom xml操作方式修改uvproj文件。

--- keil_Compiler.py
+++ keil_Compiler.py
@@ -1,74 +1,133 @@
-import os
+import os, shutil
 import sublime, sublime_plugin
 import subprocess, sys
-import xml.etree.cElementTree as ET 
+import xml.dom.minidom as minidom
 
 class KeilCompileCommand(sublime_plugin.TextCommand):
 	def run(self,edit):
 		self.view.run_command("save")
 		k=self.view.window().extract_variables()
-		os.chdir(k["project_path"])							
+		curPath = k["project_path"]
+		os.chdir(curPath)							
 		path=os.listdir(os.getcwd())
 						
 		for i in path:
-		    if(i.find(".uvproj")>0):
-        		Project=i
+			if(i.find(".uvproj")>0):
+				keilProj=os.path.join(curPath,i)
 
-        
 		settings = sublime.load_settings('keil_Compiler.sublime-settings')
 		value = settings.get('modify_uvproj')
-		if value:
-			cur_file = os.path.basename(self.view.file_name())
-			self.modify_uvproj(Project, cur_file)
-			# print("hahahhahah")
 
-		Command="UV4 -b {0} -o log.txt".format(Project)
+		if value:
+			fileList = self.getOpenFiles()
+			if not self.modify_uvproj(keilProj, fileList):
+				print ("任务终止。")
+				return
+
+
+		Command="UV4 -b {0} -o log.txt".format(keilProj)
 		
 		subprocess.call(Command,stdout=subprocess.PIPE,shell =False )
-
 		self.view.window().open_file(k["project_path"]+"/log.txt")
-		#filea = open("log.txt","r")
-		#print (filea.read())
-		print ("saved")
+		
+		print ("完成。")
 
-	# def modify_uvproj2(self, uvproj, cur_file):
+	def modify_uvproj(self, uvproj, fileList):
+		
+		if len(fileList) == 0:
+			print("已打开的文件中没有匹配的文件类型。")
+			return False
 
-	# 	tree = ET.parse(uvproj)     #打开xml文档    
+		# uvproj = "D:\lesson7.uvproj"
+		# print("UV4 proj: %s"%uvproj)
+		dom = minidom.parse(uvproj)
 
-	# 	file_node = tree.getroot().find("Targets/Target/Groups/Group/Files/File")
-	# 	# print(file_node.tag, "     ",file_node.text)
-	# 	print("修改前：",  file_node.find("FileName").text )
-	# 	file_node.find("FileName").text = cur_file
-	# 	print("修改后：",  file_node.find("FileName").text )
+		#更改hex生成选项
+		hexFile=dom.getElementsByTagName("CreateHexFile")
+		if hexFile[0].childNodes[0].nodeValue == '0':
+			hexFile[0].childNodes[0].nodeValue = '1'
+		
+		print("CreateHexFile: %s"%hexFile[0].childNodes[0].nodeValue)
 
-	# 	print("修改前：",  file_node.find("FilePath").text )
-	# 	file_node.find("FilePath").text = ".\\" + cur_file
-	# 	print("修改后：",  file_node.find("FilePath").text )
+		#删除原来的File节点
+		oldFiles = dom.getElementsByTagName("FileName")
+		for f in oldFiles:
+			print("删除旧文件信息: %s"%f.childNodes[0].nodeValue)
+			f.parentNode.parentNode.removeChild(f.parentNode); 
 
-	# 	tree.write(uvproj)	#保存
 
-	def modify_uvproj(self, uvproj, cur_file):
-		source_str1 = "<FileName>"
-		source_str2 = "<FilePath>"
-		source_str3 = "<CreateHexFile>0</CreateHexFile>"
-		source_str4 = "<OutputName>"
-		lines=open(uvproj,'r').readlines()
-		lines_len=len(lines)-1
-		for i in range(lines_len):
-			if source_str1 in lines[i]:
-				print("old: "+lines[i])
-				lines[i]= "\t\t\t  <FileName>"+cur_file+"</FileName>\n"
-				print("new: "+lines[i])
-				continue
-			if source_str2 in lines[i]:
-				print("old: "+lines[i])
-				lines[i]= "\t\t\t  <FilePath>"+cur_file+"</FilePath>\n"
-				print("new: "+lines[i])
-				continue
-			if source_str3 in lines[i]:
-				lines[i]= lines[i].replace(source_str3, "<CreateHexFile>1</CreateHexFile>")
-				continue
-			if source_str4 in lines[i]:
-				lines[i]= "\t\t\t\t\t<OutputName>"+os.path.splitext(uvproj)[0]+"</OutputName>\n"
-				continue
-		open(uvproj,'w').writelines(lines)
+		root = dom.getElementsByTagName("Files")
+
+		for f in fileList:
+			print('添加文件节点信息: %s'%f)
+			newfile = dom.createElement("File")
+
+			fileNameNode = dom.createElement("FileName")
+			fileName = dom.createTextNode(f)
+			fileNameNode.appendChild(fileName)
+
+			fileTypeNode = dom.createElement("FileType")
+			fileType = dom.createTextNode("1")
+			fileTypeNode.appendChild(fileType)
+
+			filePathNode = dom.createElement("FilePath")
+			filePath = dom.createTextNode("./"+f)
+			filePathNode.appendChild(filePath)
+			
+			newfile.appendChild(fileNameNode)
+			newfile.appendChild(fileTypeNode)
+			newfile.appendChild(filePathNode)
+
+			root[0].appendChild(newfile)
+
+		domcopy = self.beautifulFormat(dom.cloneNode(True))
+
+		uvBak = uvproj + ".uvbak"
+		shutil.copy(uvproj, uvBak)
+
+		fileHandle = open(uvproj,'w') 
+
+		try:
+
+			domcopy.writexml(fileHandle, addindent='  ', newl='\n', encoding = 'utf-8')
+			fileHandle.close() 
+			print('修改.uvproj项目文件成功。')
+
+			# 调试用
+			# i=1
+			# print("打印File信息：")
+			# for node in root[0].childNodes: 
+			# 	if node.nodeType == node.ELEMENT_NODE:	
+			# 		print("File Name %d is %s"%(i,node.getElementsByTagName("FileName")[0].childNodes[0].nodeValue))
+			# 		print("File Type %d is %s"%(i,node.getElementsByTagName("FileType")[0].childNodes[0].nodeValue))
+			# 		print("File Path %d is %s\n"%(i,node.getElementsByTagName("FilePath")[0].childNodes[0].nodeValue))
+			# 		i+=1
+		except:
+			fileHandle.close()
+			os.remove(uvproj)
+			os.rename(uvBak, uvproj)
+		finally:
+			fileHandle.close()
+			os.remove(uvBak)
+
+		return True
+	def getOpenFiles(self):
+		files=[]
+		settings = sublime.load_settings('keil_Compiler.sublime-settings')
+		value = settings.get('fileFilter')
+
+		for v in sublime.active_window().views():
+			if v and v.file_name():
+				fileName=v.file_name()
+				# print("Opened File: %s  %s"%(fileName,os.path.splitext(fileName)[1]))
+				if value and (os.path.splitext(fileName)[1] in value):	
+					files.append(os.path.basename(fileName))
+		return files
+
+	def beautifulFormat(self, xmlDomObject):
+		# '''美化xml格式
+		strXml=""
+		for line in xmlDomObject.toprettyxml(indent=' '*2).split('\n'):
+			strXml += line.strip()
+
+		return minidom.parseString(strXml)
